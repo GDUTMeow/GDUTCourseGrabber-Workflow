598897a3d436d0afe8d449e3cb48fa88cf63b8fc
GamerNoTitle <GamerNoTitle@outlook.com>
2025-06-25 20:42:31 +0800

    fix

---
+++ scripts/pack.bat
@@ -0,0 +1,39 @@
+@echo off
+chcp 65001
+
+:: 运行打包工具
+echo Running PyInstaller...
+pdm run pyinstaller ^
+    --name GDUTCourseGrabber ^
+    --noconfirm ^
+    --clean ^
+    --noupx ^
+    --log-level INFO ^
+    --add-data "static:static" ^
+    --hidden-import "uvicorn.logging" ^
+    --hidden-import "uvicorn.loops" ^
+    --hidden-import "uvicorn.loops.auto" ^
+    --hidden-import "uvicorn.protocols" ^
+    --hidden-import "uvicorn.protocols.http" ^
+    --hidden-import "uvicorn.protocols.http.auto" ^
+    --hidden-import "uvicorn.protocols.websockets" ^
+    --hidden-import "uvicorn.protocols.websockets.auto" ^
+    --hidden-import "uvicorn.lifespan" ^
+    --hidden-import "uvicorn.lifespan.on" ^
+    --hidden-import "uvicorn.lifespan.off" ^
+    --collect-all "fastapi" ^
+    --collect-all "pydantic" ^
+    --collect-all "gdut_course_grabber" ^
+    --i "static/favicon.ico" ^
+    -F src/gdut_course_grabber/__main__.py
+
+:: 将 static 文件夹复制到 dist 目录下
+echo Copying static files...
+echo "D\n" | xcopy /y /e /i static dist\static
+
+:: 清理 PyInstaller 的临时文件
+echo Cleaning up PyInstaller temporary files...
+rd /s /q build
+rd /s /q dist\GDUTCourseGrabber
+
+echo Build finished. Check the 'dist' folder.
---
+++ scripts/pack.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+echo "Cleaning previous builds..."
+mkdir -p dist/
+
+echo "Running PyInstaller..."
+pdm run pyinstaller \
+    --name GDUTCourseGrabber \
+    --noconfirm \
+    --clean \
+    --noupx \
+    --log-level INFO \
+    --add-data "static:static" \
+    --hidden-import "uvicorn.logging" \
+    --hidden-import "uvicorn.loops" \
+    --hidden-import "uvicorn.loops.auto" \
+    --hidden-import "uvicorn.protocols" \
+    --hidden-import "uvicorn.protocols.http" \
+    --hidden-import "uvicorn.protocols.http.auto" \
+    --hidden-import "uvicorn.protocols.websockets" \
+    --hidden-import "uvicorn.protocols.websockets.auto" \
+    --hidden-import "uvicorn.lifespan" \
+    --hidden-import "uvicorn.lifespan.on" \
+    --hidden-import "uvicorn.lifespan.off" \
+    --collect-all "fastapi" \
+    --collect-all "pydantic" \
+    --collect-all "gdut_course_grabber" \
+    --icon="static/favicon.ico" \
+    --onefile \
+    src/gdut_course_grabber/__main__.py
+
+echo "Cleaning up PyInstaller temporary files..."
+rm -rf build/
+
+echo "Build finished. Check the 'dist' folder."
